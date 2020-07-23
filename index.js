0347c6de9400ab692afe0e5e965aeffe309c0a56
SunJet Liu <sunjetliu@gmail.com>
2020-07-23 20:01:29 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  var upper = string.toUpperCase()
+  console.log(upper);
+}
+function logWhisper(string) {
+  var lower = string.toLowerCase()
+  console.log(lower);
+}
+function sayHiToGrandma(string){
+
+var uppercase = (string)
+
+uppercase.toUpperCase() === uppercase // true
+
+var lowercase = (string)
+
+lowercase.toLowerCase() === lowercase // true
+
+var mixedCase = (string)
+
+mixedCase.toLowerCase() === mixedCase // false
+
+mixedCase.toUpperCase() === mixedCase // false
+}
